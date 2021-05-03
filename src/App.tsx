b35f9c2a01d595182ba4ced3b9a47e8513b94cd9
Shaban elmansy <shaban.elmansy@etco.om>
2021-05-03 20:33:24 +0400

    upload file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, Fragment, useState } from "react";
+import "./App.css";
 
+type FormElem = FormEvent<HTMLFormElement>;
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
 function App() {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+  const handleSubmit = (e: FormElem) => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodo: ITodo[] = [...todos];
+    newTodo[index].complete = !newTodo[index].complete;
+    setTodos(newTodo);
+  };
+
+  const removeTodo = (index: number): void => {
+    const newTodo: ITodo[] = [...todos];
+    newTodo.splice(index, 1);
+    setTodos(newTodo);
+  };
+
+  console.log(todos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fragment>
+        <h1>Todo List</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="input"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button type="submit"> Add Todo </button>
+        </form>
+        <section>
+          {todos.map((todos: ITodo, index: number) => (
+            <Fragment key={index}>
+              <div>{todos.text}</div>
+              <button
+                type="button"
+                onClick={() => {
+                  completeTodo(index);
+                }}
+              >
+                {""}
+                {todos.complete ? "Incomplete" : "Complete"}
+                {""}
+              </button>
+              <button type="button" onClick={() => removeTodo(index)}>
+                Remove
+              </button>
+            </Fragment>
+          ))}
+        </section>
+      </Fragment>
     </div>
   );
 }
